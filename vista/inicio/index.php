41b56934e4ab755088444a7905962c181f78359e
Carlos Octavio Preciado Lafarga <carloslafarga2244@gmail.com>
2018-05-17 15:27:44 -0700

    Preparando plantilla de datos escolares
    Estamos afinando detalles en la presentacion de los datos escolares.

--- vista/inicio/index.php
+++ vista/inicio/index.php
@@ -26,7 +26,7 @@ if ($usuario == false) {
 
           <div class="wrapper wrapper-content">
               <div class="container">
-                  <div class="col-lg-4">
+                  <div class="col-lg-3">
                     <div class="ibox float-e-margins">
                         <div class="ibox-title">
                             <h5>Buscar Escuela</h5>
@@ -35,12 +35,12 @@ if ($usuario == false) {
                             </div>
                         </div>
                         <div class="ibox-content">
-                              <form class="form-horizontal">
-                                <div class="form-group">
-                                   <label class="col-lg-2 control-label">Clave:</label>
-                                    <div class="col-lg-10"><input type="text"  placeholder="Clave del centro trabajo" class="form-control">
+                            <form class="form-horizontal" >
+                              <div class="form-group">
+                                <label class="col-lg-2 control-label">Clave:</label>
+                                  <div class="col-lg-10"><input type="text" maxlength="10" placeholder="Clave CT" class="form-control">
                                     </div>
-                                </div>
+                              </div>
 
 
                                 <div class="form-group">
@@ -48,10 +48,101 @@ if ($usuario == false) {
                                         <button class="btn btn-sm btn-primary" type="submit"><i class="fa fa-search"></i>&nbsp;Buscar</button>
                                     </div>
                                 </div>
+
                             </form>
                         </div>
                     </div>
                 </div>
+
+
+                <!-- RESULTADO DE LA BUSQUEDA-->
+                <div class="col-lg-9">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>Datos de Clave:</h5>
+                            <div class="ibox-tools">
+
+                            </div>
+                        </div>
+                        <div class="ibox-content">
+                        <!--INICIO DEL CONTENT-->
+
+                        <!--DATOS ESCOLARES-->
+                        <div class="hr-line-solid"></div>
+                        <h3 class="text-danger">DATOS ESCOLARES</h3>
+                        <table class="table">
+                        <tbody>
+                        <tr>
+                            <td><label>CLAVE CT:</label>&nbsp;26DPR0001D</td>
+                            <td><label>NOMBRE CT:</label>&nbsp;SIERVO DE LA NACION</td>
+                            <td><label>TURNO:</label>&nbsp;MATUTINO</td>
+                        </tr>
+
+                        <tr>
+                            <td><label>DOMICILIO:</label>&nbsp;CUMURIPA Y PEDRO MENDEZ </td>
+                            <td><label>MUNICIPIO:</label>&nbsp;NAVOJOA</td>
+                            <td><label>LOCALIDAD:</label>&nbsp;PUEBLO MAYO</td>
+                        </tr>
+
+                        <tr>
+                            <td><label>NOMBRE DIRECTOR:</label>&nbsp;26DPR0001D</td>
+                            <td><label>TELEFONO:</label>&nbsp;6623953551</td>
+                            <td><label>CELULAR:</label>&nbsp;6421126388</td>
+                        </tr>
+
+                        </tbody>
+                        </table>
+
+                        <!--COMPARTE EDIFICIO-->
+                        <div class="hr-line-solid"></div>
+                        <h3 class="text-danger">COMPARTE EDIFICIO</h3>
+
+                           <table class="table">
+                        <tbody>
+                        <tr>
+                            <td><label>CLAVE CT:</label>&nbsp;26DPR0001D</td>
+                            <td><label>NOMBRE CT:</label>&nbsp;SIERVO DE LA NACION</td>
+                            <td><label>TURNO:</label>&nbsp;MATUTINO</td>
+                        </tr>
+
+                        <tr>
+                            <td><label>DOMICILIO:</label>&nbsp;CUMURIPA Y PEDRO MENDEZ</td>
+                            <td><label>MUNICIPIO:</label>&nbsp;NAVOJOA</td>
+                            <td><label>LOCALIDAD:</label>&nbsp;PUEBLO MAYO</td>
+                        </tr>
+
+                        <tr>
+                            <td><label>NOMBRE DIRECTOR:</label>&nbsp;26DPR0001D</td>
+                            <td><label>TELEFONO:</label>&nbsp;6623953551</td>
+                            <td><label>CELULAR:</label>&nbsp;6421126388</td>
+                        </tr>
+
+                        </tbody>
+                        </table>
+
+                        <!--EQUIPAMIENTO-->
+                        <div class="hr-line-solid"></div>
+                        <h3 class="text-danger">EQUIPAMIENTO</h3>
+
+                        <table class="table">
+                        <tbody>
+                        <tr>
+                            <td><label>EQUIPO:</label>&nbsp;25</td>
+                            <td><label>EQUIPO REAL:</label>&nbsp;25</td>
+                            <td><label>EQUIPAMIENTO:</label>&nbsp;2005</td>
+                            <td><label>REEQUIPAMIENTO:</label>&nbsp;2008</td>
+                        </tr>
+
+                        </tbody>
+                        </table>
+
+
+                        <!--FIN DEL CONTENT-->
+                        </div>
+                    </div>
+                </div>
+
+                <!-- FIN DEL RESULTADO  DE LA BUSQUEDA-->
               </div>
           </div>
 
